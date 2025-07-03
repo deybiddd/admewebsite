51369c036e2e5f1e47945f07b83a4fbba631b12d
David Alvarado <alvarado.daviddd@gmail.com>
2025-07-03 08:12:18 +0800

    Final6

--- adme-website/eslint.config.mjs
+++ adme-website/eslint.config.mjs
@@ -16,9 +16,9 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals"),
   {
     rules: {
-      '@typescript-eslint/no-unused-vars': 'warn',
-      '@typescript-eslint/no-explicit-any': 'warn',
-      'react/no-unescaped-entities': 'warn'
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'react/no-unescaped-entities': 'off'
     }
   }
 ];
